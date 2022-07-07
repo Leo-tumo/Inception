4988ae9163d9f8699903e88783a3879ef4857916
Leo-tumo <leo_tum@mail.ru>
2022-07-07 16:11:08 +0400

    ✅

--- srcs/requirements/wordpress/scripts/setup_wordpress.sh
+++ srcs/requirements/wordpress/scripts/setup_wordpress.sh
@@ -2,7 +2,7 @@
 
 while ! mariadb -h$MARIADB_HOST -P${MARIADB_PORT} -u$MARIADB_USER -p$MARIADB_PASSWORD; do echo "waiting for db ..."; done
 wp core config --dbhost=${MARIADB_HOST} --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASSWORD} --allow-root
-wp core install --url="letumany.42.fr" --title="VASTO LORDE" --admin_user="leo" --admin_password="7777" --admin_email="leonidas.xaralambos@gmail.com" --skip-email
+wp core install --url="letumany.42.fr" --title="VASTO LORDE" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
 wp plugin install hello-dolly --activate
 wp theme install twentytwenty --activate
 wp plugin update --all
