e7e49c6f986dd9aa08679fb762b1abd407ef3eb5
Leo-tumo <leo_tum@mail.ru>
2022-07-07 16:01:28 +0400

    small changes

--- srcs/requirements/wordpress/scripts/setup_wordpress.sh
+++ srcs/requirements/wordpress/scripts/setup_wordpress.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 while ! mariadb -h$MARIADB_HOST -P${MARIADB_PORT} -u$MARIADB_USER -p$MARIADB_PASSWORD; do echo "waiting for db ..."; done
-wp core install --url="jjourdan.42.fr" --title="PIKA PIKA" --admin_user="kema" --admin_password="pwd" --admin_email="kema@mgc.com" --skip-email
+wp core config --dbhost=${MARIADB_HOST} --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASSWORD} --allow-root
+wp core install --url="letumany.42.fr" --title="VASTO LORDE" --admin_user="leo" --admin_password="7777" --admin_email="leonidas.xaralambos@gmail.com" --skip-email
 wp plugin install hello-dolly --activate
 wp theme install twentytwenty --activate
 wp plugin update --all
-wp user create pikachu pikachu@pokemon.com --role=author --user_pass=pikachu
-wp post create --post_title="PIKA PIKA" --post_content="CHUUUUUU" --post_status=publish --post_author="pikachu"
+wp user create lagavulin lagavulin@islay.com --role=author --user_pass=lagavulin
+wp post create --post_title="VASTO LORDE" --post_content="ZANGETSU" --post_status=publish --post_author="lagavulin"
 
 php-fpm7 -F -R
